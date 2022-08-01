97f889a7b0e1435acab50005c65a88e8134da277
Md Ikbal Razu <37942730+ikbalrazu@users.noreply.github.com>
2022-08-01 17:31:07 +0600

    au added

--- server.js
+++ server.js
@@ -169,14 +169,14 @@ app.get("/getrecord", (req, res) => {
   let access_token_getdata;
   axios
     .post(
-      `https://accounts.zoho.com/oauth/v2/token?refresh_token=${process.env.REFRESH_TOKEN_getdata}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=refresh_token`
+      `https://accounts.zoho.com.au/oauth/v2/token?refresh_token=${process.env.REFRESH_TOKEN_getdata}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=refresh_token`
     )
     .then(function (response) {
       access_token_getdata = response.data.access_token;
     })
     .then(function (data) {
       console.log("access token created for get Record: ", access_token_getdata);
-      axios.get(`https://creator.zoho.com/api/v2/zoho_user12867/investment-portal/report/All_Users`, {
+      axios.get(`https://creator.zoho.com.au/api/v2/nickprocterau_amaltrustees2/investment-portal/report/All_Users`, {
         headers: {
           Authorization: `Zoho-oauthtoken ${access_token_getdata}`
         },
@@ -209,14 +209,14 @@ app.post("/addrecord", (req, res) => {
 
   axios
     .post(
-      `https://accounts.zoho.com/oauth/v2/token?refresh_token=${process.env.REFRESH_TOKEN_postdata}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=refresh_token`
+      `https://accounts.zoho.com.au/oauth/v2/token?refresh_token=${process.env.REFRESH_TOKEN_postdata}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=refresh_token`
     )
     .then(function (response) {
       access_token_postdata = response.data.access_token;
     })
     .then(function (data) {
       console.log("access token created for post Record: ", access_token_postdata);
-      axios.post(`https://creator.zoho.com/api/v2/zoho_user12867/investment-portal/form/User`, {
+      axios.post(`https://creator.zoho.com.au/api/v2/nickprocterau_amaltrustees2/investment-portal/form/User`, {
         data: {
           Date_Created: date,
           Email: email,
@@ -381,13 +381,13 @@ app.post("/getdocuments",(req,res)=>{
 //get all documents
 app.post("/getalldocuments",(req,res)=>{
   let access_token_getdata;
-  axios.post(`https://accounts.zoho.com/oauth/v2/token?refresh_token=${process.env.REFRESH_TOKEN_getdata}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=refresh_token`)
+  axios.post(`https://accounts.zoho.com.au/oauth/v2/token?refresh_token=${process.env.REFRESH_TOKEN_getdata}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=refresh_token`)
     .then(function (response) {
       access_token_getdata = response.data.access_token;
     })
     .then(function (data) {
       console.log("access token created for get Record: ", access_token_getdata);
-      axios.get(`https://creator.zoho.com/api/v2/zoho_user12867/investment-portal/report/All_Documents`, {
+      axios.get(`https://creator.zoho.com.au/api/v2/nickprocterau_amaltrustees2/investment-portal/report/All_Documents`, {
         headers: {
           Authorization: `Zoho-oauthtoken ${access_token_getdata}`
         },
@@ -408,13 +408,13 @@ app.post("/getalldocuments",(req,res)=>{
 //get all deals data
 app.post("/getalldeals",(req,res)=>{
   let access_token_getdata;
-  axios.post(`https://accounts.zoho.com/oauth/v2/token?refresh_token=${process.env.REFRESH_TOKEN_getdata}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=refresh_token`)
+  axios.post(`https://accounts.zoho.com.au/oauth/v2/token?refresh_token=${process.env.REFRESH_TOKEN_getdata}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=refresh_token`)
     .then(function (response) {
       access_token_getdata = response.data.access_token;
     })
     .then(function (data) {
       console.log("access token created for get Record: ", access_token_getdata);
-      axios.get(`https://creator.zoho.com/api/v2/zoho_user12867/investment-portal/report/All_Deals`, {
+      axios.get(`https://creator.zoho.com.au/api/v2/nickprocterau_amaltrustees2/investment-portal/report/All_Deals`, {
         headers: {
           Authorization: `Zoho-oauthtoken ${access_token_getdata}`
         },
@@ -581,14 +581,14 @@ app.put("/reset-password", (req, res) => {
   let access_token_updatedata;
   axios
     .post(
-      `https://accounts.zoho.com/oauth/v2/token?refresh_token=${process.env.REFRESH_TOKEN_updatedata}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=refresh_token`
+      `https://accounts.zoho.com.au/oauth/v2/token?refresh_token=${process.env.REFRESH_TOKEN_updatedata}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=refresh_token`
     )
     .then(function (response) {
       access_token_updatedata = response.data.access_token;
     })
     .then(function (data) {
       console.log("access token created for update Record: ", access_token_updatedata);
-      axios.put(`https://creator.zoho.com/api/v2/zoho_user12867/investment-portal/report/All_Users/${id}`, {
+      axios.put(`https://creator.zoho.com.au/api/v2/nickprocterau_amaltrustees2/investment-portal/report/All_Users/${id}`, {
         data: {
           Password: password
         }
