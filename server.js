8348e8e43987cef2f99b6caa849adcbdc72bdd3a
unknown <iqbalraju123@gmail.com>
2022-08-21 00:47:50 +0600

    addes cors confiq

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ const myCache = new NodeCache({stdTTL:3000});
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 // app.use(fileupload());
 app.use(bodyParser.json());
 
